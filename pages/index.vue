a9e4061122209a7d920dc293eec89a6467cce351
Johann Headley <johannheadley@Johann-Headleys-MacBook-Pro.local>
2017-05-02 13:21:55 -0400

    First commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
-      Universal Vue.js Application Framework
+      The CodeBrewers
     </h1>
     <nuxt-link class="button" to="/about">
       About page
