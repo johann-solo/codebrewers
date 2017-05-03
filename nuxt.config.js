26c36e7c743abf23e9bd8e582572a1f416ceae63
Johann Headley <johannheadley@johannhdleysmbp.lonsaatch.com>
2017-05-03 10:43:35 -0400

    Sass functionality added

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~assets/css/main.css'],
+  css: [
+    '~assets/css/main.css',
+    { src: '~assets/css/main.scss', lang: 'scss' }
+  ],
   /*
   ** Customize the progress-bar color
   */
